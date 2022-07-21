22951dc948c5d34d6f1f0138190d3d2b540dec48
EC2 Default User <ec2-user@ip-172-31-66-153.ec2.internal>
2022-07-21 03:49:06 +0000

    Lab3 Commit

--- test_lab3.py
+++ test_lab3.py
@@ -10,16 +10,16 @@ calc = Calculator()
 
 print(calc.add(1, 1))
 
-def test_add():
+def test_add_a():
     assert calc.add(1, 1) == 2
     
-def test_add():
+def test_add_b():
     assert calc.add(1.0, 2.5) == 3.5
     
-def test_add():
+def test_add_c():
     assert calc.add(0, 0) == 0
     
-def test_add():
+def test_add_d():
     assert calc.add(-5, -6) == -11
     
     
