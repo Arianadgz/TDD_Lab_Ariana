fad7f4f627658d37eaf8bc1034ea07430c1186e0
EC2 Default User <ec2-user@ip-172-31-66-153.ec2.internal>
2022-07-21 02:48:33 +0000

    Lab3 Commit

---
+++ test_lab3.py
@@ -0,0 +1,27 @@
+import pytest
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+        
+
+
+calc = Calculator()
+
+print(calc.add(1, 1))
+
+def test_add():
+    assert calc.add(1, 1) == 2
+    
+def test_add():
+    assert calc.add(1.0, 2.5) == 3.5
+    
+def test_add():
+    assert calc.add(0, 0) == 0
+    
+def test_add():
+    assert calc.add(-5, -6) == -11
+    
+    
+    
+
